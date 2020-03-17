5f29b13bd17d104c179f6db4e11e67e39c8ed198
Emil Skov Olesen <emil8250@ymail.com>
2020-03-17 12:55:21 +0100

    Created a simple roulette game, which takes Odd/Even, Red/Black/Green and a specific number.
    Closes https://github.com/Emil8250/SuperGamblino/issues/1

--- SuperGamblino/Commands/RouletteCommand.cs
+++ SuperGamblino/Commands/RouletteCommand.cs
@@ -1,6 +1,7 @@
 ﻿using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
+using SuperGamblino.GameObjects;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -14,12 +15,121 @@ namespace SuperGamblino.Commands
         [Cooldown(1, 5, CooldownBucketType.User)]
         public async Task OnExecute(CommandContext command)
         {
-            DiscordEmbed message = new DiscordEmbedBuilder
+            string[] argument = command.RawArgumentString.ToUpper().TrimStart().Split(' ');
+            bool isNumber = int.TryParse(argument[0], out int nmbGuess);
+            bool betIsNmb = int.TryParse(argument[1], out int nmbBet);
+            bool enoughCredits = true;
+
+            if (betIsNmb)
             {
-                Color = new DiscordColor(Config.colorInfo),
-                Description = "This will become a roulette command."
-            };
-            await command.RespondAsync("", false, message);
+                int curCred = Database.CommandGetUserCredits(command.User.Id);
+                if (curCred < nmbBet)
+                {
+                    enoughCredits = false;
+                    await Messages.NotEnoughCredits(command);
+                }
+                else
+                {
+                    Database.CommandSubsctractCredits(command.User.Id, nmbBet);
+                }
+                if (enoughCredits)
+                {
+                    int rewardMulti = 0;
+                    bool hasWon = false;
+                    int credWon = 0;
+                    bool invalid = false;
+                    Random random = new Random();
+                    int number = random.Next(0, 37);
+
+                    Roulette.Result result = Roulette.GetResult(number);
+
+                    if (isNumber)
+                    {
+                        if (nmbGuess == result.Number)
+                        {
+                            hasWon = true;
+                            rewardMulti = 36;
+                        }
+                    }
+                    else
+                    {
+                        switch (argument[0].ToUpper())
+                        {
+                            case "RED":
+                                if (result.Color.ToUpper() == "RED")
+                                {
+                                    hasWon = true;
+                                    rewardMulti = 2;
+                                }
+                                break;
+                            case "BLACK":
+                                if (result.Color.ToUpper() == "BLACK")
+                                {
+                                    hasWon = true;
+                                    rewardMulti = 2;
+                                }
+                                break;
+                            case "ODD":
+                                if (result.OddOrEven.ToUpper() == "ODD")
+                                {
+                                    hasWon = true;
+                                    rewardMulti = 2;
+                                }
+                                break;
+                            case "EVEN":
+                                if (result.OddOrEven.ToUpper() == "EVEN")
+                                {
+                                    hasWon = true;
+                                    rewardMulti = 2;
+                                }
+                                break;
+                            default:
+                                await Messages.InvalidArgument(command, new string[] { "<Red|Black|Odd|Even|Number> <Bet>" });
+                                invalid = true;
+                                break;
+                        }
+                    }
+
+                    string color = "";
+
+                    switch (result.Color)
+                    {
+                        case "Red":
+                            color = Config.colorWarning;
+                            break;
+                        case "Black":
+                            color = "#000000";
+                            break;
+                        case "Green":
+                            color = Config.colorSuccess;
+                            break;
+                        default:
+                            break;
+                    }
+                    string title = "";
+                    if (hasWon)
+                    {
+                        title = "Roulette - You've won!";
+                        credWon = Convert.ToInt32(argument[1]) * rewardMulti;
+                        Database.CommandGiveCredits(command.User.Id, credWon);
+                    }
+                    else
+                        title = "Roulette - You've lost!";
+
+                    DiscordEmbed message = new DiscordEmbedBuilder
+                    {
+                        Color = new DiscordColor(color),
+                        Title = title,
+                        Description = string.Format("You rolled {0} {1}.\nThe result is {2}\n\nProfit: {3}", result.Color, result.Number, result.OddOrEven, credWon)
+                    };
+                    if (!invalid)
+                    {
+                        await command.RespondAsync("", false, message);
+                    }
+                }
+            }
+            else
+                await Messages.InvalidArgument(command, new string[] { "<Red|Black|Odd|Even|Number> <Bet>" });
         }
     }
 }
---
+++ SuperGamblino/GameObjects/Roulette.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperGamblino.GameObjects
+{
+    public class Roulette
+    {
+        Result result = new Result();
+
+        public class Result
+        {
+            public string Color { get; set; }
+            public int Number { get; set; }
+            public string OddOrEven { get; set; }
+            public bool FirstEighteen { get; set; }
+        }
+
+        public static Result GetResult(int nmb)
+        {
+            string oddOrEven = nmb % 2 != 0 ? "Odd" : "Even";
+            return new Result
+            {
+                Color = GetColor(nmb),
+                Number = nmb,
+                OddOrEven = oddOrEven,
+                FirstEighteen = nmb < 19 && nmb != 0
+            };
+        }
+
+        private static string GetColor(int number)
+        {
+            List<int> redNmbs = new List<int>
+            {
+                1, 3, 5, 7, 9, 12,
+                14, 16, 18, 19, 21, 23,
+                25, 27, 30, 32, 34, 36
+            };
+            List<int> blackNmbs = new List<int>
+            {
+                2, 4, 6, 8, 10, 11,
+                13, 15, 17, 20, 22, 24,
+                26, 28, 29, 31, 33, 35
+            };
+            List<int> greenNmbs = new List<int>
+            {
+                0
+            };
+
+            if (redNmbs.Contains(number))
+                return "Red";
+            else if (blackNmbs.Contains(number))
+                return "Black";
+            else if (greenNmbs.Contains(number))
+                return "Green";
+            else
+                return "ERROR";
+        }
+    }
+}
